ba90ed5579bc159215f6b337638f61323fb415df
Dominus Iniquitatis <Iniquitatis@users.noreply.github.com>
2021-05-23 12:14:08 +0300

    Listing files manually is no longer necessary

--- Make.py
+++ Make.py
@@ -10,194 +10,14 @@
 # Configuration variables
 ################################################################################
 release_mode = False
-modded = True
 
 theme_dir  = "Themes"
 source_dir = "Source"
 build_dir  = "Build"
 
-source_common_dir = "common"
-source_mod_dir = "mas"
-
-common_files = [
-    "comfy_ui.rpy",
-    "comfy_ui/fonts/Asap-Medium.ttf",
-    "comfy_ui/fonts/Asap-MediumItalic.ttf",
-    "comfy_ui/fonts/Asap-Bold.ttf",
-    "comfy_ui/fonts/Asap-BoldItalic.ttf",
-    "comfy_ui/fonts/Nunito-SemiBold.ttf",
-    "comfy_ui/fonts/Nunito-SemiBoldItalic.ttf",
-    "comfy_ui/fonts/Nunito-Bold.ttf",
-    "comfy_ui/fonts/Nunito-BoldItalic.ttf",
-    "comfy_ui/fonts/OFL.txt",
-]
-
-theme_files = [
-    # Default
-    "info.json",
-    "comfy_ui/styles.rpy",
-    "comfy_ui/button/idle_bg_lt.svg",
-    "comfy_ui/button/idle_bg_dk.svg",
-    "comfy_ui/button/hover_bg_lt.svg",
-    "comfy_ui/button/hover_bg_dk.svg",
-    "comfy_ui/button/insensitive_bg_lt.svg",
-    "comfy_ui/button/insensitive_bg_dk.svg",
-    "comfy_ui/scrollbar/horizontal_bar_lt.svg",
-    "comfy_ui/scrollbar/horizontal_bar_dk.svg",
-    "comfy_ui/scrollbar/horizontal_idle_thumb_lt.svg",
-    "comfy_ui/scrollbar/horizontal_idle_thumb_dk.svg",
-    "comfy_ui/scrollbar/horizontal_hover_thumb_lt.svg",
-    "comfy_ui/scrollbar/horizontal_hover_thumb_dk.svg",
-    "comfy_ui/scrollbar/vertical_bar_lt.svg",
-    "comfy_ui/scrollbar/vertical_bar_dk.svg",
-    "comfy_ui/scrollbar/vertical_idle_thumb_lt.svg",
-    "comfy_ui/scrollbar/vertical_idle_thumb_dk.svg",
-    "comfy_ui/scrollbar/vertical_hover_thumb_lt.svg",
-    "comfy_ui/scrollbar/vertical_hover_thumb_dk.svg",
-    "comfy_ui/slider/horizontal_idle_thumb_lt.svg",
-    "comfy_ui/slider/horizontal_idle_thumb_dk.svg",
-    "comfy_ui/slider/horizontal_hover_thumb_lt.svg",
-    "comfy_ui/slider/horizontal_hover_thumb_dk.svg",
-    "comfy_ui/slider/vertical_idle_thumb_lt.svg",
-    "comfy_ui/slider/vertical_idle_thumb_dk.svg",
-    "comfy_ui/slider/vertical_hover_thumb_lt.svg",
-    "comfy_ui/slider/vertical_hover_thumb_dk.svg",
-
-    # Replacers
-    "comfy_ui/replacers/gui/ctc.svg",
-    "comfy_ui/replacers/gui/frame.svg",
-    "comfy_ui/replacers/gui/frame_d.svg",
-    "comfy_ui/replacers/gui/menu_bg.svg",
-    "comfy_ui/replacers/gui/menu_bg_d.svg",
-    "comfy_ui/replacers/gui/namebox.svg",
-    "comfy_ui/replacers/gui/namebox_d.svg",
-    "comfy_ui/replacers/gui/textbox.svg",
-    "comfy_ui/replacers/gui/textbox_d.svg",
-    "comfy_ui/replacers/gui/textbox_monika.svg",
-    "comfy_ui/replacers/gui/textbox_monika_d.svg",
-    "comfy_ui/replacers/gui/button/check_foreground.svg",
-    "comfy_ui/replacers/gui/button/check_foreground_d.svg",
-    "comfy_ui/replacers/gui/button/check_selected_foreground.svg",
-    "comfy_ui/replacers/gui/button/check_selected_foreground_d.svg",
-    "comfy_ui/replacers/gui/button/choice_idle_background.svg",
-    "comfy_ui/replacers/gui/button/choice_hover_background.svg",
-    "comfy_ui/replacers/gui/button/choice_dark_idle_background.svg",
-    "comfy_ui/replacers/gui/button/choice_dark_hover_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_idle_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_hover_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_disable_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_dark_idle_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_dark_hover_background.svg",
-    "comfy_ui/replacers/gui/button/scrollable_menu_dark_disable_background.svg",
-    "comfy_ui/replacers/gui/button/slot_idle_background.svg",
-    "comfy_ui/replacers/gui/button/slot_hover_background.svg",
-    "comfy_ui/replacers/gui/button/twopane_scrollable_menu_idle_background.svg",
-    "comfy_ui/replacers/gui/button/twopane_scrollable_menu_hover_background.svg",
-    "comfy_ui/replacers/gui/button/twopane_scrollable_menu_dark_idle_background.svg",
-    "comfy_ui/replacers/gui/button/twopane_scrollable_menu_dark_hover_background.svg",
-    "comfy_ui/replacers/gui/overlay/confirm.svg",
-    "comfy_ui/replacers/gui/overlay/confirm_d.svg",
-    "comfy_ui/replacers/gui/overlay/game_menu.svg",
-    "comfy_ui/replacers/gui/overlay/game_menu_d.svg",
-    "comfy_ui/replacers/gui/overlay/main_menu.svg",
-    "comfy_ui/replacers/gui/overlay/main_menu_d.svg",
-    "comfy_ui/replacers/gui/scrollbar/horizontal_poem_bar.svg",
-    "comfy_ui/replacers/gui/scrollbar/horizontal_poem_bar_d.svg",
-    "comfy_ui/replacers/gui/scrollbar/horizontal_poem_thumb.svg",
-    "comfy_ui/replacers/gui/scrollbar/vertical_poem_bar.svg",
-    "comfy_ui/replacers/gui/scrollbar/vertical_poem_bar_d.svg",
-    "comfy_ui/replacers/gui/scrollbar/vertical_poem_thumb.svg",
-    "comfy_ui/replacers/gui/slider/horizontal_hover_thumb.svg",
-    "comfy_ui/replacers/gui/slider/vertical_hover_thumb.svg",
-    "comfy_ui/replacers/mod_assets/hkb_idle_background.svg",
-    "comfy_ui/replacers/mod_assets/hkb_idle_background_d.svg",
-    "comfy_ui/replacers/mod_assets/hkb_hover_background.svg",
-    "comfy_ui/replacers/mod_assets/hkb_hover_background_d.svg",
-    "comfy_ui/replacers/mod_assets/hkb_disabled_background.svg",
-    "comfy_ui/replacers/mod_assets/hkb_disabled_background_d.svg",
-    "comfy_ui/replacers/mod_assets/island_idle_background.svg",
-    "comfy_ui/replacers/mod_assets/island_idle_background_d.svg",
-    "comfy_ui/replacers/mod_assets/island_hover_background.svg",
-    "comfy_ui/replacers/mod_assets/island_hover_background_d.svg",
-    "comfy_ui/replacers/mod_assets/music_menu.svg",
-    "comfy_ui/replacers/mod_assets/music_menu_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/idle_bg.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/idle_bg_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/hover_bg.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/hover_bg_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/insensitive_bg.svg",
-    "comfy_ui/replacers/mod_assets/buttons/generic/insensitive_bg_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_idle.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_idle_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_hover.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_hover_d.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_disabled.svg",
-    "comfy_ui/replacers/mod_assets/buttons/squares/square_disabled_d.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_close.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_close-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_close_hover.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_close_hover-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_hover_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_hover_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_disabled_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_disabled_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_name_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_day_name_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_left_arrow.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_left_arrow-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_left_arrow_hover.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_left_arrow_hover-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_right_arrow.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_right_arrow-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_right_arrow_hover.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_right_arrow_hover-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_hover_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_hover_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_disabled_bg.svg",
-    "comfy_ui/replacers/mod_assets/calendar/calendar_today_disabled_bg-n.svg",
-    "comfy_ui/replacers/mod_assets/console/cn_frame.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100_2px.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100_2px_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100_5px.svg",
-    "comfy_ui/replacers/mod_assets/frames/black70_pinkborder100_5px_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/modebar.svg",
-    "comfy_ui/replacers/mod_assets/frames/modebar_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_overlay.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_overlay_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_overlay_disabled.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_overlay_disabled_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame_selected.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame_selected_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame_disabled.svg",
-    "comfy_ui/replacers/mod_assets/frames/selector_top_frame_disabled_d.svg",
-    "comfy_ui/replacers/mod_assets/frames/trans_pink2pxborder100.svg",
-    "comfy_ui/replacers/mod_assets/frames/trans_pink2pxborder100_d.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_0.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_1.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_2.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_3.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_4.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_5.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_6.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_frame.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_frame_d.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_0.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_1.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_2.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_3.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_4.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_5.svg",
-    "comfy_ui/replacers/mod_assets/games/hangman/hm_sm_6.svg",
+mod_dirs = [
+    "common",
+    "mas",
 ]
 
 glitched_boxes = [
@@ -412,25 +232,23 @@ def PreloadThemes():
 def Log(message):
     print("BUILD: %s" % message)
 
-def ReplicateDirStructure(path_list, dst_path):
-    for path in path_list:
-        dir_path = os.path.dirname(path)
-        dir_full_path = os.path.join(dst_path, dir_path)
+def ReplicateDirStructure(file_path, dst_path):
+    dir_path = os.path.dirname(file_path)
+    dir_full_path = os.path.join(dst_path, dir_path)
 
-        if not os.path.exists(dir_full_path):
-            Log("Creating directory %s..." % dir_full_path)
-            os.makedirs(dir_full_path)
+    if not os.path.exists(dir_full_path):
+        Log("Creating directory %s..." % dir_full_path)
+        os.makedirs(dir_full_path)
 
-def FindFile(path):
-    dirs = [source_mod_dir, source_common_dir] if modded else [source_common_dir]
+def ListFilesRecursive(dir):
+    result = []
 
-    for mod_dir in dirs:
-        joined_path = os.path.join(mod_dir, path)
+    for base_path, dirs, files in os.walk(dir):
+        for file_path in files:
+            full_path = os.path.join(base_path, file_path)
+            result.append(full_path)
 
-        if os.path.exists(os.path.join(source_dir, joined_path)):
-            return joined_path
-
-    return None
+    return result
 
 def Build():
     # Clear previous build
@@ -443,64 +261,57 @@ def Build():
     os.mkdir(build_dir)
 
     # Replicate directory structure
-    ReplicateDirStructure(common_files, build_dir)
+    for mod_dir in mod_dirs:
+        main_src_dir = os.path.join(source_dir, mod_dir, "main")
+        main_files = ListFilesRecursive(main_src_dir)
 
-    # Copy common files
-    for file_path in common_files:
-        mod_file_path = FindFile(file_path)
-
-        if not mod_file_path:
-            continue
-
-        Log("Copying file %s..." % mod_file_path)
-        src_path = os.path.join(source_dir, mod_file_path)
-        dst_path = os.path.join(build_dir, file_path)
-        shutil.copyfile(src_path, dst_path)
+        # Copy common files
+        for file_path in main_files:
+            Log("Copying file %s..." % file_path)
+            rel_path = os.path.relpath(file_path, main_src_dir)
+            dst_path = os.path.join(build_dir, rel_path)
+            ReplicateDirStructure(rel_path, build_dir)
+            shutil.copyfile(file_path, dst_path)
 
     # Make themes
     themes = PreloadThemes()
 
     for theme in themes:
         for scale in range(1, 3):
-            target_id = ("%s" if scale == 1 else "%s_hidpi") % theme["id"]
-            target_dir = os.path.join(build_dir, "comfy_meta", target_id)
+            for mod_dir in mod_dirs:
+                theme_src_dir = os.path.join(source_dir, mod_dir, "theme")
+                theme_files = ListFilesRecursive(theme_src_dir)
 
-            # Replicate directory structure
-            ReplicateDirStructure(theme_files, target_dir)
+                target_id = ("%s" if scale == 1 else "%s_hidpi") % theme["id"]
+                target_dir = os.path.join(build_dir, "comfy_meta", target_id)
 
-            # Process source files
-            for file_path in theme_files:
-                (file_name, file_ext) = os.path.splitext(file_path)
+                # Process source files
+                for file_path in theme_files:
+                    rel_path = os.path.relpath(file_path, theme_src_dir)
+                    (file_name, file_ext) = os.path.splitext(rel_path)
 
-                mod_file_path = FindFile(file_path)
+                    ReplicateDirStructure(rel_path, target_dir)
 
-                if not mod_file_path:
-                    continue
-
-                if file_ext == ".svg":
-                    Log("Rendering image %s..." % mod_file_path)
-                    png_path = "%s.png" % file_name
-                    tmp_path = os.path.join(build_dir, "Temporary.svg")
-                    src_path = os.path.join(source_dir, mod_file_path)
-                    dst_path = os.path.join(target_dir, png_path)
-                    PreprocessTextFile(src_path, tmp_path, theme, scale)
-                    RenderImage(tmp_path, dst_path, scale, png_path in glitched_boxes)
-                    os.remove(tmp_path)
-                elif file_ext == ".rpy":
-                    Log("Processing script %s..." % mod_file_path)
-                    src_path = os.path.join(source_dir, mod_file_path)
-                    dst_path = os.path.join(target_dir, file_path)
-                    PreprocessTextFile(src_path, dst_path, theme, scale)
-                elif file_ext == ".json":
-                    Log("Processing JSON %s..." % mod_file_path)
-                    src_path = os.path.join(source_dir, mod_file_path)
-                    dst_path = os.path.join(target_dir, file_path)
-                    PreprocessTextFile(src_path, dst_path, theme, scale)
-                else:
-                    Log("Copying file %s..." % mod_file_path)
-                    src_path = os.path.join(source_dir, mod_file_path)
-                    dst_path = os.path.join(target_dir, file_path)
-                    shutil.copyfile(src_path, dst_path)
+                    if file_ext == ".svg":
+                        Log("Rendering image %s..." % file_path)
+                        png_path = "%s.png" % file_name
+                        tmp_path = os.path.join(build_dir, "Temporary.svg")
+                        dst_path = os.path.join(target_dir, png_path)
+                        PreprocessTextFile(file_path, tmp_path, theme, scale)
+                        RenderImage(tmp_path, dst_path, scale, png_path in glitched_boxes)
+                        os.remove(tmp_path)
+                    elif file_ext == ".rpy":
+                        Log("Processing script %s..." % file_path)
+                        dst_path = os.path.join(target_dir, rel_path)
+                        PreprocessTextFile(file_path, dst_path, theme, scale)
+                    elif file_ext == ".json":
+                        Log("Processing JSON %s..." % file_path)
+                        dst_path = os.path.join(target_dir, rel_path)
+                        PreprocessTextFile(file_path, dst_path, theme, scale)
+                    else:
+                        Log("Copying file %s..." % file_path)
+                        dst_path = os.path.join(target_dir, rel_path)
+                        shutil.copyfile(file_path, dst_path)
 
             # Pack assets
             Log("Creating archive for %s..." % target_id)
